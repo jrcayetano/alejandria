7f315a4dc9a1c6f15ea69f02a94af370be53deae
jrcayetano <josercayetano@gmail.com>
2018-09-24 21:38:26 +0200

    Added  top bar menu and scss file configuration

---
+++ src/common/header.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Menubar} from 'primereact/menubar';
+
+export class HeaderApp extends React.Component {
+  items = [
+    {label: 'Home', icon: 'pi pi-fw pi-plus'},
+    {label: 'library', icon:'pi pi-fw pi-plus' },
+    {label: 'Group', icon: 'pi pi-fw pi-trash'}
+  ];
+  render() {
+    return <Menubar model={this.items} />
+  }
+}
+/*
+export class HeaderApp extends React.Component {
+    menu = [{id: 1, value: 'home'}, {id: 2, value: 'books'},  {id: 3, value: 'groups'}];
+    render() {
+        return (
+            <header>
+              <ul>
+                <HeaderMenu menuItems={this.menu}></HeaderMenu>
+              </ul>
+            </header>
+        )
+    }
+}*/
+/*
+class HeaderMenu extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  list = hasElements(this.props.menuItems) ? 
+          this.props.menuItems.map(item => <li key={item.id}>{item.value}</li>) :
+          [];
+  render() {
+    return  this.list
+  }
+}
+
+function hasElements (array) {
+  return array && Array.isArray(array) && array.length > 0
+}*/
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
